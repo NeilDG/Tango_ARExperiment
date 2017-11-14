b36f0977527abc3bd3179470a3b223fee7b95c3d
NeilDG <NeilDG@github.com>
2017-11-14 22:38:59 +0800

    First commit of AR Tango experiment.

---
+++ Assets/Scripts/Views/MenuScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScreen : View {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnExitClicked() {
+		TwoChoiceDialog choiceDialog = (TwoChoiceDialog) DialogBuilder.Create (DialogBuilder.DialogType.CHOICE_DIALOG);
+		choiceDialog.SetMessage ("Quit the application?");
+		choiceDialog.SetOnConfirmListener (() => {
+			Application.Quit();
+		});
+	}
+
+	public override void OnRootScreenBack ()
+	{
+		base.OnRootScreenBack ();
+		this.OnExitClicked ();
+	}
+}
